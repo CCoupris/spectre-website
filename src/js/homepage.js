9d1d3b5b340a3574472fa8e8ed9263bd11b5e408
Zoé <lesouef.zoe@gmail.com>
2019-10-08 17:17:32 +0200

    HomePage Rules

---
+++ src/js/homepage.js
@@ -0,0 +1,8 @@
+// Close rules
+const button = document.querySelector('.explications_button')
+const block = document.querySelector('.explications')
+
+button.addEventListener('click',
+function(){
+    block.style.display = 'none';
+})
