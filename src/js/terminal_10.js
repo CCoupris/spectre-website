ca77a8f0d8023ca22c401dd388df4d0ff4219f6e
Zoé <lesouef.zoe@gmail.com>
2019-10-10 09:37:00 +0200

    Terminal by card

--- src/js/terminal_10.js
+++ src/js/terminal_10.js
@@ -1,11 +1,19 @@
 const button_open = document.querySelector('.button_console')
 const button_close = document.querySelector('.button_close')
 const terminal = document.querySelector('.console')
+const consoleInputBegin = document.querySelector('.console_begin_box')
+const help = document.querySelector('.help')
+let value = null
+const password_box = document.querySelector('.console_password')
 
 // Ouverture du terminal
 
 button_open.addEventListener('click', function(){
     terminal.style.display = 'block'
+    document.getElementById("message").value = "";
+    document.getElementById("password").value = "";
+    help.style.display = 'none'
+    password_box.style.display = 'none'
 })
 
 //Fermeture de la console
@@ -14,54 +22,166 @@ button_close.addEventListener('click', function(){
     terminal.style.display = 'none'
 })
 
+// Initialisation de la console 
+
+
+
 // Apparition du bouton terminal pour le perso 1
 
 const card1 = document.querySelector('.nb1')
 
 card1.addEventListener('click', function(){
     button_open.style.display = 'flex'
+    startTerminal01()
 })
 
 
-// Validation du terminal de commande
+//Fonction du terminal en fonction du personnage
 
+function startTerminal01 (){
+    const consoleInputBegin = document.querySelector('.console_begin_box')
+    const help = document.querySelector('.help')
+    let value = null
+    const password_box = document.querySelector('.console_password')
 
-const consoleInputBegin = document.querySelector('.console_begin_box')
-const help = document.querySelector('.help')
-let value = null
-const password_box = document.querySelector('.console_password')
-
-
-consoleInputBegin.addEventListener('keydown', function(_event)
-{
-    value = consoleInputBegin.value
-
-    if(_event.key === 'Enter' && value === '/help')
+    // Terminal de commande
+    consoleInputBegin.addEventListener('keydown', function(_event)
     {
-        help.style.display = 'block'
-        
-    }
-    if(_event.key === 'Enter' &&  value ===  'pwd'){
-        password_box.style.display = 'flex'
-    }
+        value = consoleInputBegin.value
+ 
+        console.log(value)
+        if(_event.key === 'Enter' && value === '/help')
+        {
+            help.style.display = 'block'
+            
+        }
+        if(_event.key === 'Enter' &&  value ===  'pwd'){
+            password_box.style.display = 'flex'
+        }
+    })
+
+    // Validation du mot de passe 
+    const  consoleInputPassword = document.querySelector('.console_password_box')
+    let value_password = null
+    const wrong = document.querySelector('.wrong')
+    const right = document.querySelector('.true')
+
+    consoleInputPassword.addEventListener('keydown', function(_event){
+        value_password = consoleInputPassword.value
+
+        if(_event.key === 'Enter' && value_password === 'metal' || value_password === 'Metal'){
+            wrong.style.display= 'none'
+            right.style.display = 'block'
+        }else if(_event.key == 'Enter' && value_password !=  'metal' || value_password === 'Metal'){
+            wrong.style.display = 'block'
+            right.style.display = 'none'
+        }
+    })
+}
+
+//Card 2
+const card2 = document.querySelector('.nb2')
+
+card2.addEventListener('click', function(){
+    button_open.style.display = 'flex'
+    startTerminal02()
 })
 
-// Validation du mot de passe
 
-const  consoleInputPassword = document.querySelector('.console_password_box')
-let value_password = null
-const wrong = document.querySelector('.wrong')
-const right = document.querySelector('.true')
+//Fonction du terminal en fonction du personnage
 
-consoleInputPassword.addEventListener('keydown', function(_event){
-    value_password = consoleInputPassword.value
+function startTerminal02 (){
+    const consoleInputBegin = document.querySelector('.console_begin_box')
+    const help = document.querySelector('.help')
+    let value = null
+    const password_box = document.querySelector('.console_password')
 
-    if(_event.key === 'Enter' && value_password === 'metal' || value_password === 'Metal'){
-        wrong.style.display= 'none'
-        right.style.display = 'block'
-    }else if(_event.key == 'Enter' && value_password !=  'bonjour'){
-        wrong.style.display = 'block'
-        right.style.display = 'none'
-    }
+    // Terminal de commande
+    consoleInputBegin.addEventListener('keydown', function(_event)
+    {
+        value = consoleInputBegin.value
+
+        if(_event.key === 'Enter' && value === '/help')
+        {
+            help.style.display = 'block'
+            
+        }
+        if(_event.key === 'Enter' &&  value ===  'pwd'){
+            password_box.style.display = 'flex'
+        }
+    })
+
+    // Validation du mot de passe 
+    const  consoleInputPassword = document.querySelector('.console_password_box')
+    let value_password = null
+    const wrong = document.querySelector('.wrong')
+    const right = document.querySelector('.true')
+
+    consoleInputPassword.addEventListener('keydown', function(_event){
+        value_password = consoleInputPassword.value
+
+        if(_event.key === 'Enter' && value_password === 'Mardi' || value_password === 'mardi'){
+            wrong.style.display= 'none'
+            right.style.display = 'block'
+        }else if(_event.key == 'Enter' && value_password != 'Mardi' || value_password != 'mardi'){
+            wrong.style.display = 'block'
+            right.style.display = 'none'
+        }
+    })
+}
+
+const card10 = document.querySelector('.nb10')
+
+card2.addEventListener('click', function(){
+    button_open.style.display = 'flex'
+    startTerminal10()
 })
+
+function startTerminal10 (){
+    const consoleInputBegin = document.querySelector('.console_begin_box')
+    const help = document.querySelector('.help')
+    let value = null
+    const password_box = document.querySelector('.console_password')
+
+    // Terminal de commande
+    consoleInputBegin.addEventListener('keydown', function(_event)
+    {
+        value = consoleInputBegin.value
+
+        if(_event.key === 'Enter' && value === '/help')
+        {
+            help.style.display = 'block'
+            
+        }
+        if(_event.key === 'Enter' &&  value ===  'pwd'){
+            password_box.style.display = 'flex'
+        }
+    })
+
+    
+    // Validation du mot de passe 
+    const  consoleInputPassword = document.querySelector('.console_password_box')
+    let value_password = null
+    const wrong = document.querySelector('.wrong')
+    const right = document.querySelector('.true')
+
+    consoleInputPassword.addEventListener('keydown', function(_event){
+        value_password = consoleInputPassword.value
+
+        if(_event.key === 'Enter' && value_password === 'Metal' || value_password === 'metal'){
+            wrong.style.display= 'none'
+            right.style.display = 'block'
+        }else if(_event.key == 'Enter' && value_password != 'Metal' || value_password != 'metal'){
+            wrong.style.display = 'block'
+            right.style.display = 'none'
+        }
+    })
+}
+
+
+
+
+
+
+
 
